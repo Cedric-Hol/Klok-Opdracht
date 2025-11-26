cf29dc176cc154b70e542ae9f4f9059370985186
Cedric <Cedric@IHOPEMENOBROKEY>
2025-11-26 13:27:39 +0100

    The clock is finaly done

--- Klok-Opdracht/Form1.cs
+++ Klok-Opdracht/Form1.cs
@@ -16,20 +16,34 @@ namespace Klok_Opdracht
     {
         public Form1()
         {
+            
             InitializeComponent();
         }
 
         Point p0, p1, p2, p3;
         static int S = 150;
         static double offset = Math.PI / 2;
+        /// <summary>
+        /// Starts the timer
+        /// </summary>
         private void button1_Click(object sender, EventArgs e)
         {
-            p0 = new Point(this.Width / 2, this.Height / 2);
             timer1.Start();
         }
 
+        /// <summary>
+        /// Timer that updates every second
+        /// </summary>
+        /// <algo>
+        /// Determines the cennter of the form and makes it p0
+        /// clears the form to draw a new clock
+        /// Draws the clockface and numbers
+        /// makes the wisers for seconds, minutes and hours
+        /// </algo>
         private void timer1_Tick(object sender, EventArgs e)
         {
+            p0 = new Point(this.Width / 2, this.Height / 2);
+            this.CreateGraphics().Clear(BackColor);
             int second = DateTime.Now.Second;
             int minute = DateTime.Now.Minute;
             int hour = DateTime.Now.Hour;
@@ -39,9 +53,16 @@ namespace Klok_Opdracht
             wiser(minute, 30, Color.Blue, p2);
             wiser(hour, 6, Color.Red, p3);
         }
+        /// <summary>
+        /// Draws a wiser based on the time in color c
+        /// </summary>
+        /// <algo>
+        /// First calculates the Angle F based on the n(number) and t(time) minus the offset
+        /// It calculates the O(opposite) and A(adjacent) using S(sizeness) and F(angle)
+        /// then it calculates the new point p based on p0(center) plus A and O and draws a line from p0 to p
+        /// </algo>
         private void wiser(int t, int n, Color C, Point p)
         {
-            this.CreateGraphics().DrawLine(new Pen(BackColor, 2), p0, p);
             double F = Math.PI / n * t - offset;
             double O = (int)(S * Math.Sin(F));
             double A = (int)(S * Math.Cos(F));
@@ -50,37 +71,3 @@ namespace Klok_Opdracht
         }
     }
 }
-/*
-private void second_hand()
-{
-    this.CreateGraphics().DrawLine(new Pen(BackColor, 2), p0, p1);
-    int second = DateTime.Now.Second;
-    double F = Math.PI / 30 * second - offset;
-    double O = (int)(S * Math.Sin(F));
-    double A = (int)(S * Math.Cos(F));
-    p1 = new Point((int)(p0.X + A), (int)(p0.Y + O));
-    this.CreateGraphics().DrawLine(new Pen(Color.Orange, 2), p0, p1);
-}
-
-private void minute_hand()
-{
-    this.CreateGraphics().DrawLine(new Pen(BackColor, 2), p0, p2);
-    int minute = DateTime.Now.Minute;
-    double F = Math.PI / 30 * minute - offset;
-    double O = (int)(S * Math.Sin(F));
-    double A = (int)(S * Math.Cos(F));
-    p2 = new Point((int)(p0.X + A), (int)(p0.Y + O));
-    this.CreateGraphics().DrawLine(new Pen(Color.Blue, 2), p0, p2);
-}
-
-private void hour_hand()
-{
-    this.CreateGraphics().DrawLine(new Pen(BackColor, 2), p0, p3);
-    int hour = DateTime.Now.Hour;
-    double F = Math.PI / 6 * hour - offset;
-    double O = (int)(S * Math.Sin(F));
-    double A = (int)(S * Math.Cos(F));
-    p3 = new Point((int)(p0.X + A), (int)(p0.Y + O));
-    this.CreateGraphics().DrawLine(new Pen(Color.Red, 2), p0, p3);
-}
-*/
--- Klok-Opdracht/klokface.cs
+++ Klok-Opdracht/klokface.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -10,24 +11,29 @@ namespace Klok_Opdracht
 {
     public class klokface
     {
+        /// <summary>
+        /// Draws the clockface with numbers around it
+        /// </summary>
+        /// <algo>
+        /// Determens the radius and diameter of the clockface
+        /// Then draws the circle at the center w,h with the diameter
+        /// then it starts calculating the postion of the numbers
+        /// And draws them around the clockface
+        /// </algo>
         public static void draw_clockface(int w, int h, int s, Form f)
         {
             Graphics g = f.CreateGraphics();
             int radius = s + 100 / 2;
             int diameter = 2 * radius;
             g.DrawEllipse(new Pen(Color.Black, 3), w - radius, h - radius, diameter, diameter);
+            s+=30;
+            for (int i = 1; i <= 12; i++)
+            {
+                double angle = i * Math.PI / 6 - Math.PI / 2;
+                int x = (int)(w + s * Math.Cos(angle));
+                int y = (int)(h + s * Math.Sin(angle));
+                g.DrawString(i.ToString(), new Font("Arial", 15), Brushes.Black, x - 10, y - 10);
+            }
         }
-
-
-
-        /*
-        public static void remove_clockface(int w, int h, int s, Form f)
-        {
-            Graphics g = f.CreateGraphics();
-            int radius = s + 100 / 2;
-            int diameter = 2 * radius;
-            g.DrawEllipse(new Pen(f.BackColor, 3), w - radius, h - radius, diameter, diameter);
-        }
-        */
     }
 }
